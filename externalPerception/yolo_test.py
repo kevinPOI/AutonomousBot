4e8c8b0b35dabeafa70bd0393456fb2c33b0ce19
kevinPOI <kevin_tang233@outlook.com>
2024-07-23 13:59:40 -0400

    added separate yolo test demo

---
+++ externalPerception/yolo_test.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+from PIL import Image
+import cv2
+import time
+import numpy as np
+
+# model = YOLO("yolov8n.pt")
+model = YOLO("robotv3_10e.pt")
+camera = cv2.VideoCapture("nhrl_sample2.mp4")
+for i in range(1300):
+    
+    ret, og_frame = camera.read()
+    if(i > 5):
+        t0 = time.perf_counter()
+        results = model.predict(og_frame, show = True, verbose = False)
+        print("infer time:", time.perf_counter() - t0)
